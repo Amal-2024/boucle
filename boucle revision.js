6bb91bec4f532261ce6a6a247058ea628d20b042
Amal-2024 <amalrherari@gmail.com>
2024-11-21 15:42:59 +0100

    Add files via upload

---
+++ boucle revision.js
@@ -0,0 +1,39 @@
+//Eng: Use a for loop to print numbers from 1 to 10.
+//○ Fr: Utilisez une boucle for pour afficher les nombres de 1 à 10
+
+//for (let i=1;i<=10;i++)
+   //{ 
+    //console.log(i)}
+
+  // Use a for loop to print the square of each number from 1 to 5.
+//○ Fr: Utilisez une boucle for pour afficher le carré de chaque nombre de 1 
+//for (let i=1;i<=5;i++){
+   // console.log(i*i)
+//}
+
+
+//let i=5
+//while (i>=1) {
+   
+// console.log (i);
+   
+// i--;
+
+//}
+    
+//let color=["red", "green", "bleu"]
+ //color.forEach(function(color){
+   // console.log(color);
+
+ 
+//for (let i=1;i<=15;i++)
+   // if( i % 2 !== 0){
+   // console.log( i)
+//}
+let produit=1;
+for(let i=1;i<=5;i++){
+    produit *=i;
+}
+        console.log (produit);
+   
+    
