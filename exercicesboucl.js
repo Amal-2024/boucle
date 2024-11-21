ff1c9d459cc799e53fca5ee75fff64cc2b4a1032
Amal-2024 <amalrherari@gmail.com>
2024-11-21 15:40:02 +0100

    Add files via upload

---
+++ exercicesboucl.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=30;i++){
+    console.log(i)
+}
+
+let elements =["banana","fraise","poivre"]
+    elements.forEach(function(elements){
+        console.log(elements.toUpperCase())
+    });
+        
+    
